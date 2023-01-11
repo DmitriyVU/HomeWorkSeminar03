3ad92c42b50e3e310584d4ef3f8805fa414c1d03
DmitriyVU <Dmitriy.vladimirovich.u@gmail.com>
2023-01-11 20:00:33 +0300

    добавил решение task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает 
+ на вход пятизначное число и проверяет, является ли 
+ оно палиндромом.
+
+ 14212 -> нет
+ 23432 -> да
+ 12821 -> да */
+Console.Clear();
+
+Console.Write("Введите любое пятизначное число -");
+int num = int.Parse(Console.ReadLine());
+if (num < 0)
+{
+    num *= -1;
+}
+if (num >= 10000 && num < 100000)
+{
+    int A = num / 10000;
+    int B = num / 1000 % 10;
+    int C = num % 100 / 10;
+    int D = num % 10;
+    if (A == D && B == C)
+    {
+        Console.WriteLine($"{num} - да! палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{num} - НЕ палиндром!");
+    }
+}
+else
+{
+    Console.WriteLine($"{num} - НЕ пятизначное!?!");
+}
